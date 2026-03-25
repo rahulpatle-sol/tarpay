4414f5fe6115e5b3dac5fadde17573c7b8167b28
rahulpatle-sol <patler044@gmail.com>
2026-03-25 19:21:56 +0530

    old apk  removed

--- tarpay-premium/hooks/useTheme.js
+++
@@ -1,7 +0,0 @@
-import { useColorScheme } from "react-native";
-import { Light, Dark } from "../constants/theme";
-export function useTheme() {
-  const scheme = useColorScheme();
-  const isDark = scheme === "dark";
-  return { C: isDark ? Dark : Light, isDark };
-}
